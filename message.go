321f8fc0bcd09447f7eca3dd8737bbf86f3ce5e1
Brad Bowman <beeradb@gmail.com>
2016-07-28 16:49:49 -0600

    [severity] Add severity

--- message.go
+++ message.go
@@ -1,5 +1,13 @@
 package drudsub
 
+const (
+	SeverityFatal   = "FATAL"
+	SeverityError   = "ERROR"
+	SeverityWarning = "WARNING"
+	SeverityInfo    = "INFO"
+	SeverityDebug   = "DEBUG"
+)
+
 // Message represents a Pub/Sub/NATS message.
 type Message struct {
 	Data       []byte
